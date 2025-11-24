5ec630ae1f9d51b45a2ca98d9ab925209b1db31a
aktaruzzamanjoti97 <aktaruzzamanjoti97@gmail.com>
2025-11-24 07:27:34 +0600

    done

--- lib/auth.ts
+++ lib/auth.ts
@@ -1,16 +1,7 @@
 import { betterAuth } from 'better-auth';
-import { mongodbAdapter } from 'better-auth/adapters/mongodb';
 import { nextCookies } from 'better-auth/next-js';
-import { MongoClient } from 'mongodb';
-
-const client = new MongoClient('mongodb://localhost:27017/database');
-const db = client.db();
 
 export const auth = betterAuth({
-	database: mongodbAdapter(db, {
-		// Optional: pass client for transactions
-		client,
-	}),
 	emailAndPassword: {
 		enabled: true,
 	},
